4a05b5014a75b8b0695bb8360dd4c7670c0e1c52
iamaashk <adnanahmadshaikh2003@gmail.com>
2024-02-19 00:26:23 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import logo from './logo.svg';
+import { uploadFile } from './services/api';
 import './App.css';
+import { useRef, useState, useEffect } from 'react';
 
 function App() {
+  const [file, setFile] = useState('');
+  const [result,setResult] = useState('');
+  const fileInputRef = useRef();
+
+  const logo = 'https://i.pinimg.com/originals/16/46/24/1646243661201a0892cc4b1a64fcbacf.jpg'
+
+  useEffect(()=>{
+    const getImage = async ()=>{
+      if(file){
+        const data = new FormData();
+        data.append("name", file.name);
+        data.append("file",file);
+
+       let response = await uploadFile(data);
+       setResult(response.path);
+      }
+  }
+  getImage();
+  },[file])
+  
+  const onUploadClick=()=>{
+    fileInputRef.current.click();
+  }
+  const onChange = (e) => {
+    setFile(e.target.files[0]);
+  }
+  console.log(file);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "container">
+    
+        <img src = {logo} alt ="banner" />
+        <div className='wrapper'>
+          <h1>Simple File Sharing</h1>
+          Upload and Share the Download Link
+
+          <button onClick={()=>onUploadClick()}>Upload</button>
+          <input type="file"
+              ref={fileInputRef}
+              style={ {display:'none'}}
+              onChange={(e) => setFile(e.target.files[0])}
+          />
+
+          <a href={result} target = "_blank">{result}</a>
+        </div>
     </div>
   );
 }
